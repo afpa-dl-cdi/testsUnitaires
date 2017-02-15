b918796c29f297e91694812e2d3ee76f6f7eaf29
david di Marcantonio <david@code4mac.com>
2017-02-15 11:50:20 +0100

    exemples de tests

--- testsUnitaires/Program.cs
+++ testsUnitaires/Program.cs
@@ -6,10 +6,14 @@ using System.Threading.Tasks;
 
 namespace testsUnitaires
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            int resultat = MyMath.Addition(2, 2);
+            Fortune fortune = new Fortune();
+            string aleat = fortune.phrase(3);
         }
     }
 }
+ 
